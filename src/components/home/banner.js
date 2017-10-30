2dc88c9068a2732d300c3bd3f4940d54b80c874c
gdegas001 <gdegas001@gmail.com>
2017-10-30 16:25:12 -0700

    Changed styles.#3

--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const banner = () => {
   return (
-    <div className="banner-wrapper">
-      <header className="banner-header">
-        <h1>ATLERNATIVE MUSIC POOL</h1>
+    <div className="banner-wrapper container-fluid">
+      <header className="banner-header text-center">
+        <h1>ALTERNATIVE MUSIC POOL</h1>
         <div className="banner-sub-header">
           <h4>A DISK JOCKEY ONE STOP FOR ALTERNATIVE DJ EDITS</h4>
         </div>
